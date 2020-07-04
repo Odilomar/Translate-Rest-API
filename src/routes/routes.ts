c6324fd3d8436194d593e838d5ac4f9b2914bcb3
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-07-03 22:13:08 -0400

    :art: Valid post params and set root/output dir files from ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 
 import TranslateService from "../services/TranslateService";
+import TranslatedText from "../interface/TranslatedInterface";
 
 const router = Router();
 
@@ -8,8 +9,13 @@ async function translate(request: Request, response: Response) {
     const { textToTranslate } = request.body;
     const modelId: string = request.path.replace('/', '');
 
-    const translateEnPtService: TranslateService = new TranslateService();
+    if (!textToTranslate) {
+        const errorTranslation: TranslatedText = { status: 400, statusText: 'Invalid request. See the response body for details!', text: ''};
 
+        return response.json(errorTranslation);
+    }
+
+    const translateEnPtService: TranslateService = new TranslateService();
     const translatedText = await translateEnPtService.execute(textToTranslate, modelId);
 
     return response.json(translatedText);
@@ -17,6 +23,6 @@ async function translate(request: Request, response: Response) {
 
 router.post('/en-pt', translate);
 
-// router.get('/pt-en', );
+router.post('/pt-en', translate);
 
 export default router;
